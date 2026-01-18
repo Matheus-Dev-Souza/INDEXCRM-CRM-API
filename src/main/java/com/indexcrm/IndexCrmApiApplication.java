66a311c9cbea4b4519d8a4994a3aef0b81f27b48
Matheus-Dev-Souza <goncalvesouza1996@gmail.com>
2026-01-17 23:49:49 -0300

    first commit

---
+++ src/main/java/com/indexcrm/IndexCrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.indexcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexCrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexCrmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indexcrm/IndexCrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indexcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexCrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
