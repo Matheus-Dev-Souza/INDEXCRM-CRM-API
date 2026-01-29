1f6b1f685d2e6a278d320aa8a397c6f89249da72
Mateus <goncalvesouza1996@gmail.com>
2026-01-29 20:32:08 -0300

    feat: funções do PlanRepository

--- src/main/java/com/indexcrm/repository/plan/PlanRepository.java
+++ src/main/java/com/indexcrm/repository/plan/PlanRepository.java
@@ -1,6 +1,14 @@
 package com.indexcrm.repository.plan;
 
-public class PlanRepository {
+import com.indexcrm.domain.plan.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface PlanRepository extends JpaRepository<Plan, Long> {
     
+    // Método mágico do Spring Data: Monta a query "WHERE active = true" sozinho
+    List<Plan> findByActiveTrue();
 }
