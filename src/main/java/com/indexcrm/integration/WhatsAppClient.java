1c82b668e05d06ff497764d2b1050b8744a9df0f
Mateus <goncalvesouza1996@gmail.com>
2026-01-29 21:33:06 -0300

    refactor: ajustando erro no whats

--- src/main/java/com/indexcrm/integration/WhatsAppClient.java
+++ src/main/java/com/indexcrm/integration/WhatsAppClient.java
@@ -1,6 +1,11 @@
 package com.indexcrm.integration;
 
-public class WhatsAppClient {
+public interface WhatsAppClient {
 
-    
+    /**
+     * Envia uma mensagem de texto simples.
+     * @param phone Número no formato internacional (Ex: 5511999999999)
+     * @param message Texto da mensagem
+     */
+    void sendText(String phone, String message);
 }
