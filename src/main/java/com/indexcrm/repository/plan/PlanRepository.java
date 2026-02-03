9313d5f1c70cfb9849c750c7a3a431f1612b9374
Mateus <goncalvesouza1996@gmail.com>
2026-02-03 10:48:35 -0300

    refactor

--- src/main/java/com/indexcrm/repository/plan/PlanRepository.java
+++ src/main/java/com/indexcrm/repository/plan/PlanRepository.java
@@ -1,14 +1,5 @@
 package com.indexcrm.repository.plan;
 
-import com.indexcrm.domain.plan.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+public interface PlanRepository  {
 
-import java.util.List;
-
-@Repository
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-    
-    // Método mágico do Spring Data: Monta a query "WHERE active = true" sozinho
-    List<Plan> findByActiveTrue();
 }
