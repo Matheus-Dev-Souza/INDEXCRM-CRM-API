f55f26e0f0f285694034eddfcf9af0248fc4fe29
Mateus <goncalvesouza1996@gmail.com>
2026-02-03 11:27:22 -0300

    refactor

--- src/main/java/com/indexcrm/repository/automation/AutomationFlowRepository.java
+++ src/main/java/com/indexcrm/repository/automation/AutomationFlowRepository.java
@@ -1,15 +1,5 @@
 package com.indexcrm.repository.automation;
 
-import com.indexcrm.domain.automation.AutomationFlow;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+public interface AutomationFlowRepository  {
 
-import java.util.List;
-
-@Repository
-public interface AutomationFlowRepository extends JpaRepository<AutomationFlow, Long> {
-
-    // CORREÇÃO: Mudamos de (Long companyId) para (String companyId)
-    // para bater com o ID da sua Empresa que é UUID.
-    List<AutomationFlow> findByCompanyId(String companyId);
 }
