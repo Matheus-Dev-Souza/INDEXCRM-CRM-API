126eab6cccd4436b389e82fb9df082ddea6ddd37
Mateus <goncalvesouza1996@gmail.com>
2026-02-03 11:24:31 -0300

    refactor

--- src/main/java/com/indexcrm/service/plan/PlanService.java
+++ src/main/java/com/indexcrm/service/plan/PlanService.java
@@ -1,27 +1,7 @@
 package com.indexcrm.service.plan;
 
-import com.indexcrm.domain.plan.Plan;
-import com.indexcrm.repository.plan.PlanRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-@Service
 public class PlanService {
-
-    @Autowired
-    private PlanRepository planRepository;
-
-    public List<Plan> getAllActivePlans() {
-        return planRepository.findByActiveTrue();
-    }
-
-    public Plan getPlanById(Long id) {
-        return planRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Plano não encontrado"));
-    }
-
 
 
 }
